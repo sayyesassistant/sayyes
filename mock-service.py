e09107448e6d6342bfb3eff5f52c21f59c2418e8
igor <igor.p.almeida@gmail.com>
2013-12-02 13:50:18 -0200

    CHANGE: mock service for travell updated

--- mock-service.py
+++ mock-service.py
@@ -51,11 +51,71 @@ class DestiniationForm(webapp2.RequestHandler):
                 "exception" : None,
                 "message" : None,
                 "value" : {
-                    "title":"Destino "+destination,
-                    "description":"Obrigado por seu interesse.",
-                    "text":"Em breve um de nossos atendentes entrará em contato com você."
+                    "title" : "Obrigado,",
+                    "description" : "Quando você gostaria de ir para {}?".format(destination),
+                    "text":"Em breve um de nossos atendentes entrará em contato com você.",
+                    "form" : {
+                        "action" : "/mock-service/destination-result.json",
+                        "method" : "POST",
+                        "id" : "get-application",
+                        "on_success" : "result",
+                        "on_error" : "error",
+                        "hiddens" : [{
+                          "value":destination,
+                          "name":"destination"
+                        }],
+                        "inputs":[{
+                            "name":"name",
+                            "type":"text",
+                            "placeholder":"Qual o seu nome?",
+                            "required" : "required"
+                        },{
+                            "name":"email",
+                            "type":"email",
+                            "placeholder":"Qual o seu email?",
+                            "required" : "required"
+                        },{
+                            "name":"date-start",
+                            "type":"date",
+                            "placeholder":"quando gostaria de ir?",
+                            "required" : "required"
+                        },{
+                            "name":"date-end",
+                            "type":"date",
+                            "placeholder":"quantos gostadia de voltar?",
+                            "required" : "required"
+                        }],
+                        "buttons" : [{
+                            "label":"pedir orçamento de viagem",
+                            "value":"receber"
+                        }]
+                    }
                 }
             }
+        self.response.out.write(json.dumps(obj))
+
+    def get(self):
+        self.validate(self)
+
+    def post(self):
+        self.validate(self)
+
+class DestiniationResultForm(webapp2.RequestHandler):
+    def validate(self, *args):
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.headers['Access-Control-Allow-Methods'] = 'POST, GET'
+        c_name = self.request.get('name')
+        c_dest = self.request.get('destination')
+        char_set = string.ascii_uppercase + string.digits
+        obj = {
+            "status" : "success",
+            "exception" : None,
+            "message" : None,
+            "value" : {
+                "title" : "Obrigado {},".format(c_name),
+                "description" : "O registro do seu atendimento para {}\né: {}".format(c_dest.upper(),''.join(random.sample(char_set*6,25)))
+            }
+        }
         self.response.out.write(json.dumps(obj))
 
     def get(self):
@@ -106,5 +166,6 @@ class Foo(webapp2.RequestHandler):
 application = webapp2.WSGIApplication([
     (r'/mock-service/login.json', LoginForm)
     ,(r'/mock-service/destination.json', DestiniationForm)
+    ,(r'/mock-service/destination-result.json', DestiniationResultForm)
     ,(r'/mock-service/request-view.json', ViewAuthForm)
     , (r'/mock-service/foo.json', Foo)], debug=True)
